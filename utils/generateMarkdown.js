11dfd9c143c1dbdc85bd4c604c92839f62f97749
philcurtis422 <philcurtis422@gmail.com>
2024-07-15 19:06:32 -0400

    finished generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,46 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title} ${licenseBadge}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents 
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.install}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${licenseSection}
+
+  ## How to Contribute
+
+  ${data.contrib}
+
+  ## Tests
+
+  ${data.test}
+
+  ## Questions
+
+  If you have any questions my github username is : ${data.github}  
+  And the link to my github is : https://github.com/${data.github}  
+  And finally you can email me at: ${data.email}
+
 
 `;
 }
