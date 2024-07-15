816e136884ddb8f155ed1d74171d3fb19bbdcf3b
philcurtis422 <philcurtis422@gmail.com>
2024-07-15 19:08:54 -0400

    added to generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -32,6 +32,8 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
   return `
   # ${data.title} ${licenseBadge}
 
