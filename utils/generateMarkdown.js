0fb42914ff08435b1d3fbde371efdfbdf1810924
philcurtis422 <philcurtis422@gmail.com>
2024-07-15 19:07:46 -0400

    finished render license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,16 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+        return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+        return 'https://www.apache.org/licenses/LICENSE-2.0';
+    default:
+        return '';
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
